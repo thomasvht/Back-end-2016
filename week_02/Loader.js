986c1cb0b6b4bcd8a6b254093f7c7cf75ec2f53a
thomas vanhoutte <thomas.vanhoutte@student.howest.be>
2016-10-07 15:39:04 +0200

    + de versie van Johan

--- week_02/Loader.js
+++ week_02/Loader.js
@@ -2,23 +2,51 @@
 * @Author: thomasvanhoutte
 * @Date:   2016-10-07T15:09:14+02:00
 * @Last modified by:   thomasvanhoutte
-* @Last modified time: 2016-10-07T15:16:46+02:00
+* @Last modified time: 2016-10-07T15:38:23+02:00
 */
 
 "use strict";
 
-var Loader = (function(){
-  //private vars
-  var startTime = startTime? startTime: new Date.getTime();
 
-  //private methods
-  var duration = function () { return (new Date.getTime() - startTime);};
-  var loadAsync = function(element, callback){};
-  var loadArrayAsync = function (array, elements, callback){};
-  return{
-    //public methods
-    loadArrayAsync: loadArrayAsync,
-  };
+var Loader = (function () {
+    //private vars
+    var startTime = startTime? startTime: new Date().getTime();
+
+    //private methods
+    var duration = function () { return (new Date().getTime() - startTime); };
+
+    var loadAsync = function (element , cb) {
+        if (element === "ERROR") {
+            cb("ERROR", null);
+        } else {
+            setTimeout(function () { cb(null, element + " is loaded"); }, 1000);
+        }
+    };
+    var loadArrayAsync = function (arrayA , elements, cb) {
+        startTime = new Date().getTime(); //reïnitialize
+        var counter = 0;
+        for (var element in elements) {
+            //element is een index bij for lus
+            var sElement = elements[element];
+            loadAsync(sElement, function (err, element) {
+                counter++;
+                if (err) {
+                    arrayA[counter] = element;
+                    cb(err, null);
+                } else {
+                    arrayA[counter] = element; //undefined
+                    console.log(arrayA[counter]);
+                    if (counter === elements.length) {
+                        cb(null, arrayA , duration());
+                    }
+                }
+            });
+        }
+    };
+    return {
+        //public methods
+        loadArrayAsync: loadArrayAsync,
+    };
 })();
 
-module.exports = Loader;
+module.exports = Loader; 
--- week_02/Loader_oefening.js
+++ week_02/Loader_oefening.js
@@ -2,19 +2,22 @@
 * @Author: thomasvanhoutte
 * @Date:   2016-10-07T15:05:10+02:00
 * @Last modified by:   thomasvanhoutte
-* @Last modified time: 2016-10-07T15:16:36+02:00
+* @Last modified time: 2016-10-07T15:38:43+02:00
 */
 
-var Loader = require("./Loader.js");
+var Loader = require("./modLoader.js");
 
 var users = [];
-var userIDs = ["P1", "P2", "P3", "ERROR", "P5", "P6", "P7", "P8", "P9"];
+var usersIds = ["P1", "P2", "P3", "P4", "ERROR", "P6", "P7", "P8", "P9"];
 
-Loader.loadArrayAsync(users , usersIds, function (err, arr, duration) {
-  if(err){
-    console.log(err);
-  }
-  else {
-    console.log("Doorlooptijd van de Loader module:", duration, " ms");
-  }
+
+Loader.loadArrayAsync(users, usersIds, function (err, arr, duration) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Doorlooptijd van de Loader module:" , duration);
+    }
 });
+
+
+setTimeout(function () { process.exit(); } , 15000);
